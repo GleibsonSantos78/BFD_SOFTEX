736a04cbdbff705129b6ed99906edb74d4898bbf
GleibsonSantos78 <gleibson.jose.dos.santos@gmail.com>
2025-08-27 18:44:47 +0000

    Atualizado!

---
+++ AULAS_MODULO_01/AULA_05/01 - MATRIZ.py
@@ -0,0 +1,38 @@
+# # matriz = [[1,2,3],[4,5,6],[7,8,9]]
+
+# # print(matriz[1][1])
+# # print(matriz.index)
+
+# # print(matriz)
+
+# matriz2 = [[1,2,3],[4,5,6],[7,8,9]]
+
+# for linha in matriz2:
+#     for elemento in linha:
+#         print(f"{elemento:2}",  end=" ")
+# print()
+
+
+
+
+# lista1 = []
+
+# for i in range(10):
+#     lista1.append(2)
+
+# print(lista1)
+
+# for i in range(10):
+#     lista1.append(i*2)
+
+# print(lista1)
+
+#lista3 = (for i in range(10):lista1.append(2))
+
+#lista4 = (for linha in range(3) for coluna in range(3))
+
+
+
+
+matriz2 = [[1,2,3],[4,5,6],[7,8,9]]
+print(f" {matriz2[0]}","\n",f"{matriz2[1]}","\n",f"{matriz2[2]}","\n",)
---
+++ AULAS_MODULO_01/AULA_06/02 - INSTALACAO VENV.py
@@ -0,0 +1,8 @@
+
+# instalar bm Python -m venv .venv
+
+# online python.exe -m pip install --upgrade pip
+#python.exe -m django install --upgrade django
+#pip install djangocms-installer
+#pip list
+#source .vm\active 
