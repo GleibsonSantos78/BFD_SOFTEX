4f05c0040087f17841f9a152f2b5002c5ffefbc5
GleibsonSantos78 <gleibson.jose.dos.santos@gmail.com>
2025-09-04 23:20:18 +0000

    !

--- AULAS_MODULO_02/AULA_02/Exercicio_04.py
+++ AULAS_MODULO_02/AULA_02/Exercicio_04.py
@@ -1,10 +1,7 @@
 #Implemente um programa que abra um arquivo chamado dados.txt (não precisa existir). Use try e finally para garantir que uma mensagem de "Encerrando programa" seja sempre exibida.
 
 try:
-    abrir_arquivo = open("Gleibson.txt, "r")
-    ler_arquivo = abrir_arquivo.read()
-except:
-print("")
-finally:
-print(f"")
-                         
+    abrir_arquivo = open("Gleibson.txt", "r")
+    ler_arquivo = Gleibson.read()
+    print("Conteúdo do arquivo:")
+print(conteudo)
---
+++ AULAS_MODULO_02/AULA_02/Exercicio_05.py
@@ -0,0 +1,1 @@
+#Crie uma função dividir(a, b) que lance (raise) uma exceção ZeroDivisionError se b for igual a zero. Caso contrário, retorne o resultado da divisão.
