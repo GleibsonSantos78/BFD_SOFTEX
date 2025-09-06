9a0c161d533099ff2c1e5f466ca5cdabee8eccdb
GleibsonSantos78 <gleibson.jose.dos.santos@gmail.com>
2025-09-06 02:30:03 +0000

    Atualizado!

--- AULAS_MODULO_02/EXERCICIOS_FUNCOES/Exercicio_03.py
+++ AULAS_MODULO_02/EXERCICIOS_FUNCOES/Exercicio_03.py
@@ -1,8 +1,10 @@
-#Crie uma função chamada soma que receba dois números e retorne a soma deles. Depois, use a função para somar 10 + 20.
-def soma(num1, num2):
-    print(num1+num2)
-soma(5,6)
+#Crie uma função chamada soma que receba dois números e retorne
+# a soma deles. Depois, use a função para somar 10 + 20.
 
-def soma(num1, num2):
-    return(num1+num2)
-soma(10,20)
+def soma(num1,num2):
+    return num1 + num2
+
+valor1 = 10
+valor2 = 20
+
+print(soma(valor1,valor2))
